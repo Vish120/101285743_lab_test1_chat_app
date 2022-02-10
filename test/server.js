68f19fed1cba3affbe469e31aa7b052eee33fdbd
Vish120 <90358096+Vish120@users.noreply.github.com>
2022-02-09 22:35:42 -0500

    Delete test directory

--- test/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var module = require('node modules');
-var server = app.listen(3000, () => {
-    console.log('server is running on port', server.address().port)
-})
-
-app.use(express.static(__dirname));
